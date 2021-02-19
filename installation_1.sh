f44caf8f49b5ebe136eede98f93f7c323d03b232
Philipp Bielefeldt <p.bielefeldt@secure.mailbox.org>
2021-02-19 09:36:15 +0100

    add some flatpak installs

--- installation_1.sh
+++ installation_1.sh
@@ -19,7 +19,10 @@ zypper ar -f http://download.opensuse.org/repositories/science/$DISTNAME/ "Scien
 #sudo zypper ar -f http://download.opensuse.org/repositories/Application:/Geo/$DISTNAME/ "Geo"
 zypper ar -f http://download.opensuse.org/repositories/Application:/Geo:/OpenCPN/$DISTNAME/ "openCPN/Geo"
 zypper ar -f http://download.opensuse.org/repositories/Application:/Geo/$DISTNAME/ "ZYGrib/Geo"
+
 #zypper ar -f http://download.opensuse.org/repositories/isv:ownCloud:desktop/$DISTNAME/isv:ownCloud:desktop.repo "isvOwnCloud"
+rpm --import https://download.owncloud.com/desktop/ownCloud/stable/latest/linux/openSUSE_Leap_15.2/repodata/repomd.xml.key
+zypper ar -f https://download.owncloud.com/desktop/ownCloud/stable/latest/linux/${DISTNAME}/ "OwnCloud"
 #zypper ar -r https://mega.nz/linux/MEGAsync/$DISTNAME/ "MegaSync"
 
 ### Changes to the system are only of continued effect if you trust the keys
@@ -40,13 +43,14 @@ gnome-logs \
 go go-doc \
 gummi \
 epiphany gnome-shell-search-provider-epiphany \
+evince-plugin-djvudocument \
 htop \
 iotop \
 ipe \
 julia julia-examples \
 latexila \
 texlive-scheme-basic \
-texlive-animate texlive-appendixnumberbeamer texlive-bbm texlive-bbm-macros texlive-boxedminipage texlive-cleveref texlive-fontspec texlive-glossaries texlive-glossaries-english texlive-glossaries-german texlive-import texlive-newtx texlive-standalone texlive-todonotes texlive-units texlive-xtab \
+texlive-animate texlive-appendixnumberbeamer texlive-bbm texlive-bbm-macros texlive-bbold texlive-boxedminipage texlive-cleveref texlive-fontspec texlive-glossaries texlive-glossaries-english texlive-glossaries-german texlive-import texlive-newtx texlive-standalone texlive-todonotes texlive-units texlive-xtab \
 texlive-biber \
 libgda \
 links \
@@ -54,6 +58,7 @@ meld \
 mlocate \
 ncdu \
 OpenCPN-4.x-Base \
+opi \
 owncloud-client \
 pandoc \
 pidgin libpurple-plugin-skypeweb \
@@ -67,7 +72,7 @@ supertuxkart \
 whois \
 wxMaxima \
 xmoto \
-xournal \
+xournalpp \
 zygrib \
 
 
@@ -89,5 +94,13 @@ zygrib \
 ### Some nice themes
 #sudo zypper in gtk3-metatheme-elementary
 #sudo zypper in gtk3-metatheme-greybird
+
+echo "... done... "
+
+echo "install flatpaks ..."
+
+flatpak install flathub org.onlyoffice.desktopeditors
+flatpak install flathub org.zulip.Zulip
+flatpak install flathub com.spotify.Client
 
 echo "... done... "
